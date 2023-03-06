7d11ea4b418489a98618a9fcff8fb45620555051
trinity31 <kyeongyeon.song@gmail.com>
2023-03-06 10:30:18 +0900

    Remove unnecessary code

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,4 @@
 import { useRouter } from "next/router";
-//import { getPostData, getPostsFiles } from "../../lib/posts-util";
 import PostContent from "./post-detail/post-content";
 
 export default function PostDetailPage(props) {
@@ -9,26 +8,3 @@ export default function PostDetailPage(props) {
 
   return <PostContent post={decodedPost} />;
 }
-
-// export function getStaticProps(context) {
-//   const { params } = context;
-//   const { slug } = params;
-
-//   const postData = getPostData(slug);
-
-//   return {
-//     props: {
-//       post: postData,
-//     },
-//     revalidate: 600,
-//   };
-// }
-
-// export function getStaticPaths() {
-//   const postFileNames = getPostsFiles();
-//   const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ""));
-//   return {
-//     paths: slugs.map((slug) => ({ params: { slug: slug } })),
-//     fallback: false,
-//   };
-// }
