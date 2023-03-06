afa7ee1af7cdbf226cd45acb127ea0ca2515cf71
trinity31 <kyeongyeon.song@gmail.com>
2023-03-06 18:19:00 +0900

    Added adsense script

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import Head from "next/head";
 import FeaturedPosts from "@/components/home-page/featured-posts";
 import { getFeaturedNotionPages } from "@/lib/notion-util";
+import Script from "next/script";
 
 export default function HomePage(props) {
   return (
@@ -12,6 +13,11 @@ export default function HomePage(props) {
           name="description"
           content="Blog about essential oils and aromatherapy"
         />
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.ADS_CLIENT_ID}`}
+          crossorigin="anonymous"
+        ></Script>
       </Head>
       <FeaturedPosts posts={props.posts} />
     </>
