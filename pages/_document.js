0bf43f4ae7e54e99e8b62e00ac53e3739c14f233
trinity31 <kyeongyeon.song@gmail.com>
2023-04-03 13:28:11 +0900

    Fix adsense script issue

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+export default function MyDocument() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Aroma Journey</title>
+        <meta
+          name="description"
+          content="Blog about essential oils and aromatherapy"
+        />
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.ADS_CLIENT_ID}`}
+          crossorigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,11 @@
-import { Fragment } from "react";
-import Head from "next/head";
 import FeaturedPosts from "@/components/home-page/featured-posts";
 import { getFeaturedNotionPages } from "@/lib/notion-util";
-import Script from "next/script";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 
 export default function HomePage(props) {
-  const { t } = useTranslation("common");
-
   return (
     <>
-      <Head>
-        <title>{t("aroma_journey")}</title>
-        <meta
-          name="description"
-          content="Blog about essential oils and aromatherapy"
-        />
-      </Head>
-      <Script
-        async
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.ADS_CLIENT_ID}`}
-        crossorigin="anonymous"
-      ></Script>
       <FeaturedPosts posts={props.posts} />
     </>
   );
